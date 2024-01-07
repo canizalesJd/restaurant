face099efe4fe756a14e3fcac10f4e0944bfbebe
Jose Canizales <canizalesjd@gmail.com>
2024-01-06 18:36:01 -0600

    Refactoring layout and adding initial content to home

--- src/index.js
+++ src/index.js
@@ -29,17 +29,13 @@ for (item in menuItems) {
 	menu.appendChild(menuItem);
 }
 
-// main
-const mainContent = document.createElement("div");
-mainContent.classList.add("main-content");
-content.appendChild(mainContent);
-
 // Tab holder
 const tabHolder = document.createElement("div");
-mainContent.appendChild(tabHolder);
+content.appendChild(tabHolder);
 tabHolder.classList.add("tab-holder");
 
 // Footer
+
 const footer = document.createElement("footer");
 const footerText = document.createElement("p");
 // Gett current year
@@ -48,3 +44,49 @@ footerText.innerHTML += `Developed by <a href="https://github.com/canizalesJd" t
 footer.classList.add("footer");
 footer.appendChild(footerText);
 content.appendChild(footer);
+
+// Home tab
+const homeTab = document.createElement("div");
+homeTab.classList.add("tab");
+homeTab.classList.add("home");
+
+const homeTab_aboutUs = document.createElement("div");
+homeTab_aboutUs.classList.add("about-us");
+homeTab.appendChild(homeTab_aboutUs);
+
+const aboutUs_title = document.createElement("h2");
+aboutUs_title.classList.add("large-title");
+aboutUs_title.textContent = "Discover Gourmet Paradise";
+homeTab_aboutUs.appendChild(aboutUs_title);
+
+const homeDescription = document.createElement("p");
+homeDescription.textContent =
+	"Immerse yourself in the epitome of French culinary artistry at our exquisite restaurant. Indulge in a symphony of flavors meticulously crafted with the finest ingredients, where each dish is a celebration of gastronomic sophistication.";
+homeTab_aboutUs.appendChild(homeDescription);
+
+const menuHighlights = document.createElement("div");
+menuHighlights.classList.add("menu-highlights");
+homeTab.appendChild(menuHighlights);
+
+const menuHighlights_title = document.createElement("h2");
+menuHighlights_title.classList.add("medium-title");
+menuHighlights_title.textContent = "Menu Highlights";
+menuHighlights.appendChild(menuHighlights_title);
+
+const menuHighlights_description = document.createElement("p");
+menuHighlights_description.textContent =
+	"Explore our curated menu featuring signature dishes like our Gourmet Grilled Filet, a harmonious blend of delicate textures and bold flavors. Experience decadence with Royal Sea Bass Elegance, showcasing our commitment to gastronomic excellence. Each creation is a masterpiece, promising an unforgettable dining experience.";
+menuHighlights.appendChild(menuHighlights_description);
+
+const menuHighlights_images = document.createElement("div");
+menuHighlights.appendChild(menuHighlights_images);
+
+const menuHighlights_images_image1 = document.createElement("img");
+menuHighlights_images_image1.src = "img/filet.jpg";
+menuHighlights_images.appendChild(menuHighlights_images_image1);
+
+const menuHighlights_images_image2 = document.createElement("img");
+menuHighlights_images_image2.src = "img/fish.jpg";
+menuHighlights_images.appendChild(menuHighlights_images_image2);
+
+tabHolder.appendChild(homeTab);
